cb46592dfdd762749c81455d43436b937cfaf71d
Taylor Savage <tjsavage@glados.stanford.edu>
2012-04-03 21:43:17 -0700

    Added purchase to admin

--- deals/admin.py
+++ deals/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from deals.models import Deal, Campus
+from deals.models import Deal, Campus, Purchase
 
 admin.site.register(Deal)
 admin.site.register(Campus)
+admin.site.register(Purchase)
